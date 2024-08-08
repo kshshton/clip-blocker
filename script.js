7dfddedb6f2f9ab415fd6dca7de5891d7b69c32c
kshshton <96781803+kshshton@users.noreply.github.com>
2024-08-08 22:24:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,59 +9,57 @@
 // @grant        none
 // ==/UserScript==
 
-(async function() {
-    'use strict';
+(async function () {
+  "use strict";
 
-    const regex = /[ąćęłńóśżźĄĆĘŁŃÓŚŻŹ]/g;
-    const polishChars = {
-        'ą': 'a',
-        'ć': 'c',
-        'ę': 'e',
-        'ł': 'l',
-        'ń': 'n',
-        'ó': 'o',
-        'ś': 's',
-        'ż': 'z',
-        'ź': 'z',
-        'Ą': 'A',
-        'Ć': 'C',
-        'Ę': 'E',
-        'Ł': 'L',
-        'Ń': 'N',
-        'Ó': 'O',
-        'Ś': 'S',
-        'Ż': 'Z',
-        'Ź': 'Z'
-    };    
+  const regex = /[ąćęłńóśżźĄĆĘŁŃÓŚŻŹ]/g;
+  const polishChars = {
+    ą: "a",
+    ć: "c",
+    ę: "e",
+    ł: "l",
+    ń: "n",
+    ó: "o",
+    ś: "s",
+    ż: "z",
+    ź: "z",
+  };
 
-    const blackList = prompt('Write keywords separated with ","')
-        .toLowerCase()
-        .replaceAll(regex, match => polishChars[match])
-        .split(",")
-        .map(keyword => keyword.trim());
+  const blackList = prompt('Write keywords separated with ","')
+    .toLowerCase()
+    .replaceAll(regex, (match) => polishChars[match])
+    .split(",")
+    .map((keyword) => keyword.trim());
 
-    const execute = () => {
-        return Array.from(document.querySelectorAll(
-            "#contents ytd-video-renderer, ytd-rich-item-renderer, ytd-grid-video-renderer"
-        ))
-            .map(element => ({DOMElement: element, h3: element.querySelector("h3")}))
-            .filter(element => element.h3 != null)
-            .map(element =>
-                ({
-                    ...element,
-                    h3: element.h3.textContent.replaceAll('\n', '')
-                        .trim()
-                        .toLowerCase()
-                        .replaceAll(regex, match => polishChars[match])
-                 }))
-            .filter(element => blackList.some(keyword => element.h3.includes(keyword)))
-            .forEach(element => element.DOMElement.remove());
-    }
-    
-    const wait = seconds => new Promise(_ => setTimeout(_, seconds * 1000));
-    
-    while(true) {
-        await wait(1);
-        execute();
-    }
+  const execute = () => {
+    return Array.from(
+      document.querySelectorAll(
+        "#contents ytd-video-renderer, ytd-rich-item-renderer, ytd-grid-video-renderer"
+      )
+    )
+      .map((element) => ({
+        DOMElement: element,
+        h3: element.querySelector("h3"),
+      }))
+      .filter((element) => element.h3 != null)
+      .map((element) => ({
+        ...element,
+        h3: element.h3.textContent
+          .replaceAll("\n", "")
+          .trim()
+          .toLowerCase()
+          .replaceAll(regex, (match) => polishChars[match]),
+      }))
+      .filter((element) =>
+        blackList.some((keyword) => element.h3.includes(keyword))
+      )
+      .forEach((element) => element.DOMElement.remove());
+  };
+
+  const wait = (seconds) => new Promise((_) => setTimeout(_, seconds * 1000));
+
+  while (true) {
+    await wait(1);
+    execute();
+  }
 })();
